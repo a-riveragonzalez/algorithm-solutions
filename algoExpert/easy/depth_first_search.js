bb543adfcec3ee9c4417d1f0e9943285f5713268
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-02-10 14:02:54 -0800

    added depth first search

---
+++ algoExpert/easy/depth_first_search.js
@@ -0,0 +1,25 @@
+// https://www.algoexpert.io/questions/depth-first-search
+
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      // Write your code here.    
+      array.push(this.name) 
+      for (let child of this.children) {
+        child.depthFirstSearch(array)
+      }
+      return array
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.Node = Node;
